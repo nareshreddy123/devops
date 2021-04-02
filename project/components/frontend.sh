6af89ddd3f958c7be367c056c10a2c495fd6ae86
Naresh REDDY <nareshreddyboreddy904@gmail.com>
2021-04-02 16:01:42 +1100

    this is my Makefile

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-USER_ID=$(id -u)
-if [ "${USER_ID}" -ne 0 ]; then
-  echo -e "\e[1;31mYou should be a root user to perform this command\e[0m"
-  exit 1
-fi
-
-
-
-
 
 yum install nginx -y
